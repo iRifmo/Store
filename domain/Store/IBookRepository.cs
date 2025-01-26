4170c029ec2f3ac1af1a5612e2d1fbafb2251a8a
iRifmo <FullDogma@gmail.com>
2025-01-26 15:33:12 +0700

    Промежуточный коммит 1

--- domain/Store/IBookRepository.cs
+++ domain/Store/IBookRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Store
 {
-    internal interface IBookRepository
+    public interface IBookRepository
     {
         Book[] GetAllByTitle(string titlePart);
     }
