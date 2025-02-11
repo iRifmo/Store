f14726fcf4745f3a863ce4290d1ba00cdb5473b2
iRifmo <FullDogma@gmail.com>
2025-02-12 02:28:04 +0700

    Реализовать поиск по ISBN и автору

--- presentation/Store.Wen/Controllers/SearchController.cs
+++ presentation/Store.Wen/Controllers/SearchController.cs
@@ -4,18 +4,18 @@ namespace Store.Wen.Controllers
 {
     public class SearchController : Controller
     {
-        private readonly IBookRepository bookRepository;
+        private readonly BookService bookService;
 
-        public SearchController(IBookRepository bookRepository)
+        public SearchController(BookService bookService)
         {
-            this.bookRepository = bookRepository;
+            this.bookService = bookService;
         }
         public IActionResult Index(string query)
         {
 
-            var books = bookRepository.GetAllByTitle(query);
+            var books = bookService.GetAllByQuery(query);
 
-            return View();
+            return View(books);
         }
     }
 }
--- presentation/Store.Wen/Program.cs
+++ presentation/Store.Wen/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddSingleton<IBookRepository, BookRepository>();
+builder.Services.AddSingleton<BookService>();
+
 
 var app = builder.Build();
 
