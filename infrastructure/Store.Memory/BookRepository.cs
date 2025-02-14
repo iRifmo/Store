bdf2d47b0a8d21e17d66e0d2a68ef5b3d683d44c
iRifmo <FullDogma@gmail.com>
2025-02-15 01:37:13 +0700

    Добавить страницу описания книги, добавить цену книги.

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -7,9 +7,12 @@ namespace Store.Memory
     {
         private readonly Book[] books = new[]
         {
-            new Book(1, "ISBN 12312-31231", "D. Knuth", "Art Of Programming"),
-            new Book(2, "ISBN 12312-31232", "M. Fowler", "Refactoring"),
-            new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language"),
+            new Book(1, "ISBN 12312-31231", "D. Knuth", "Art Of Programming", 
+                        "Description of book number 1", 7.19M),
+            new Book(2, "ISBN 12312-31232", "M. Fowler", "Refactoring", 
+                        "Description of book number 2", 12.45M),
+            new Book(3, "ISBN 12312-31233", "B. Kernighan, D. Ritchie", "C Programming Language",
+                        "Description of book number 3", 14.98M),
         };
 
         public Book[] GetAllByIsbn(string isbn)
@@ -23,6 +26,11 @@ namespace Store.Memory
             return books.Where(book => book.Author.Contains(query)
                                     || book.Title.Contains(query))
                         .ToArray();
+        }
+
+        public Book GetById(int id)
+        {
+            return books.Single(book => book.Id == id);
         }
     }
 }
--- presentation/Store.Wen/Controllers/HomeController.cs
+++ presentation/Store.Wen/Controllers/HomeController.cs
@@ -18,11 +18,6 @@ namespace Store.Wen.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
